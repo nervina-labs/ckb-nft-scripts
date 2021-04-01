c6afe660da63e59bc1d13b1797e56b90f9dc0d4e
duanyytop <duanyytop@gmail.com>
2021-04-01 10:58:05 +0800

    refactor: Add constant to issuer struct

--- contracts/script-utils/src/issuer.rs
+++ contracts/script-utils/src/issuer.rs
@@ -1,7 +1,9 @@
 use crate::error::Error;
-use crate::helper::{parse_dyn_vec_len, u32_from_slice};
+use crate::helper::{parse_dyn_vec_len, u32_from_slice, DYN_MIN_LEN};
 use core::result::Result;
 
+const FIXED_LEN: usize = 9;
+// FIXED_LEN + DYN_MIN_LEN
 const ISSUER_DATA_MIN_LEN: usize = 11;
 pub const ISSUER_TYPE_ARGS_LEN: usize = 20;
 
@@ -30,11 +32,10 @@ impl Issuer {
         }
 
         let class_count = u32_from_slice(&data[1..5]);
-        let set_count = u32_from_slice(&data[5..9]);
+        let set_count = u32_from_slice(&data[5..FIXED_LEN]);
 
-        let info_len = parse_dyn_vec_len(&data[9..11]);
-        // The min length of the issuer data is: 9(the length of fixed data) + info_len
-        if data.len() < info_len + 9 {
+        let info_len = parse_dyn_vec_len(&data[FIXED_LEN..(FIXED_LEN + DYN_MIN_LEN)]);
+        if data.len() < info_len + FIXED_LEN {
             return Err(Error::IssuerDataInvalid);
         }
 
