5647000053af8a4d8b813c321284662dec496b8d
duanyytop <duanyytop@gmail.com>
2021-11-02 09:40:26 +0800

    feat: Verify smt proof of compact_nft

--- contracts/compact-nft-type/src/entry.rs
+++ contracts/compact-nft-type/src/entry.rs
@@ -1,15 +1,21 @@
+use alloc::vec::Vec;
 use ckb_std::high_level::load_cell_data;
 use ckb_std::{
     ckb_constants::Source,
     ckb_types::{bytes::Bytes, packed::*, prelude::*},
+    dynamic_loading_c_impl::CKBDLContext,
     high_level::{load_cell, load_cell_lock_hash, load_cell_type, load_script},
 };
 use core::result::Result;
-use nft_smt::transfer::{ClaimCompactNFTEntries, CompactNFTId};
-use script_utils::compact_nft::CompactNft;
+use nft_smt::{
+    smt::blake2b_256,
+    transfer::{ClaimCompactNFTEntries, CompactNFTId},
+};
 use script_utils::{
+    compact_nft::CompactNft,
     error::Error,
     helper::{load_class_type_with_args, load_group_input_witness_args_with_type},
+    smt::LibCKBSmt,
 };
 
 const TYPE_ARGS_LEN: usize = 20;
@@ -17,11 +23,29 @@ const MINT_CLAIM: u8 = 1;
 const TRANSFER_WITHDRAW: u8 = 2;
 const TRANSFER_CLAIM: u8 = 3;
 const COMPACT_NFT_ID_LEN: usize = 29;
+const REVERSED: [u8; 3] = [0u8; 3];
 
 fn check_type_args_not_equal_lock_hash(type_: &Script, source: Source) -> Result<bool, Error> {
     let lock_hash = load_cell_lock_hash(0, source)?;
     let type_args: Bytes = type_.args().unpack();
     Ok(type_args[..] != lock_hash[0..TYPE_ARGS_LEN])
+}
+
+fn check_output_compact_nft_type(compact_nft_type: &Script) -> Result<(), Error> {
+    // Outputs[0] must be compact_nft_cell whose type_args must be equal the lock_hash[0..20]
+    let output_compact_type = load_cell_type(0, Source::Output).map_err(|_e| Error::Encoding)?;
+    match output_compact_type {
+        Some(type_) => {
+            if compact_nft_type.as_slice() != type_.as_slice() {
+                return Err(Error::CompactCellPositionError);
+            }
+            if check_type_args_not_equal_lock_hash(&type_, Source::Output)? {
+                return Err(Error::CompactTypeArgsNotEqualLockHash);
+            }
+            Ok(())
+        }
+        None => return Err(Error::CompactCellPositionError),
+    }
 }
 
 fn check_class_cell_dep(nft_id: &CompactNFTId) -> Result<(), Error> {
@@ -41,22 +65,17 @@ fn check_class_cell_dep(nft_id: &CompactNFTId) -> Result<(), Error> {
 }
 
 fn validate_type_and_verify_smt(compact_nft_type: &Script) -> Result<(), Error> {
-    // Outputs[0] must be compact_nft_cell whose type_args must be equal the lock_hash[0..20]
-    let output_compact_type = load_cell_type(0, Source::Output).map_err(|_e| Error::Encoding)?;
-    match output_compact_type {
-        Some(type_) => {
-            if compact_nft_type.as_slice() != type_.as_slice() {
-                return Err(Error::CompactCellPositionError);
-            }
-            if check_type_args_not_equal_lock_hash(&type_, Source::Output)? {
-                return Err(Error::CompactTypeArgsNotEqualLockHash);
-            }
-        }
-        None => return Err(Error::CompactCellPositionError),
-    }
+    check_output_compact_nft_type(&compact_nft_type)?;
 
     let compact_nft = CompactNft::from_data(&load_cell_data(0, Source::Output)?[..])?;
     let witness_args = load_group_input_witness_args_with_type(&compact_nft_type)?;
+
+    let mut context = unsafe { CKBDLContext::<[u8; 128 * 1024]>::new() };
+    let lib_ckb_smt = LibCKBSmt::load(&mut context);
+    let mut keys: Vec<u8> = Vec::new();
+    let mut values: Vec<u8> = Vec::new();
+    let mut proof: Vec<u8> = Vec::new();
+
     if let Some(witness_args_type) = witness_args.input_type().to_opt() {
         let witness_args_input_type: Bytes = witness_args_type.unpack();
         if compact_nft.nft_smt_root.is_none() {
@@ -73,13 +92,47 @@ fn validate_type_and_verify_smt(compact_nft_type: &Script) -> Result<(), Error> 
                     .ok_or(Error::Encoding)
                     .map_err(|_e| Error::Encoding)?;
                 check_class_cell_dep(&nft_id)?;
+                for index in 0..owned_nft_ids.len() {
+                    let owned_nft_id = owned_nft_ids
+                        .get(index)
+                        .ok_or(Error::Encoding)
+                        .map_err(|_e| Error::Encoding)?;
+                    let claimed_nft_key = claim_entries
+                        .claimed_nft_keys()
+                        .get(index)
+                        .ok_or(Error::Encoding)
+                        .map_err(|_e| Error::Encoding)?;
+                    keys.extend(&REVERSED);
+                    keys.extend(owned_nft_id.as_slice());
+                    keys.extend(Vec::from(blake2b_256(claimed_nft_key.as_slice())));
+
+                    let owned_nft_values = claim_entries
+                        .owned_nft_values()
+                        .get(index)
+                        .ok_or(Error::Encoding)
+                        .map_err(|_e| Error::Encoding)?;
+                    let claimed_nft_values = claim_entries
+                        .owned_nft_values()
+                        .get(index)
+                        .ok_or(Error::Encoding)
+                        .map_err(|_e| Error::Encoding)?;
+                    values.extend(owned_nft_values.as_slice());
+                    values.extend(claimed_nft_values.as_slice());
+                }
+
+                proof = claim_entries.proof().raw_data().to_vec();
+                if let Some(compact_smt_root) = compact_nft.nft_smt_root {
+                    lib_ckb_smt
+                        .smt_verify(&compact_smt_root[..], &keys[..], &values[..], &proof[..])
+                        .map_err(|_| Error::SMTProofVerifyFailed)?;
+                }
             }
             _ => {}
         }
     }
 
     // If the inputs[0] is compact_nft_cell, then its type_args must be equal to
-    // lock_hash[0..20] and its lock script must be equal to outputs.compact_nft_cell.
+    // lock_hash[0..20] and its smt proof must be verified successfully.
     if let Some(type_) = load_cell_type(0, Source::Input)? {
         if compact_nft_type.as_slice() != type_.as_slice() {
             return Ok(());
@@ -87,8 +140,16 @@ fn validate_type_and_verify_smt(compact_nft_type: &Script) -> Result<(), Error> 
         if check_type_args_not_equal_lock_hash(&type_, Source::Input)? {
             return Err(Error::CompactTypeArgsNotEqualLockHash);
         }
+        if !proof.is_empty() {
+            let input_compact_nft = CompactNft::from_data(&load_cell_data(0, Source::Input)?[..])?;
+            values.clear();
+            if let Some(compact_smt_root) = input_compact_nft.nft_smt_root {
+                lib_ckb_smt
+                    .smt_verify(&compact_smt_root[..], &keys[..], &values[..], &proof[..])
+                    .map_err(|_| Error::SMTProofVerifyFailed)?;
+            }
+        }
     };
-
     Ok(())
 }
 
